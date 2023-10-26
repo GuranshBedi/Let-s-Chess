fc0159e635c56f81aad6171e30a7febbe9550722
GuranshBedi <141507285+GuranshBedi@users.noreply.github.com>
2023-10-26 08:54:40 +0530

    Add files via upload

---
+++ Chess.js
@@ -0,0 +1,23 @@
+
+gsap.from(".nav",{
+    y:10,
+    duration:1,
+    delay: 1,
+    opacity:0,
+    stagger:0.1
+})
+gsap.from(".Heading",{
+    y:10,
+    duration:1,
+    opacity:0
+})
+gsap.from(".black-box",{
+    y:10,
+    duration:1,
+    opacity:0
+})
+gsap.from(".white-box",{
+    y:10,
+    duration:1,
+    opacity:0
+})
